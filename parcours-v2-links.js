6bf2deec379923d1f8065ca6f4706435069f3ba1
🌌 CelestialCode <87706832+PLL-process@users.noreply.github.com>
2026-07-07 22:28:48 -0400

    Supprimer l’ancien script de redirection des séances

--- parcours-v2-links.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(() => {
-  const links = [...document.querySelectorAll('a[href="seance-7.html"],a[href="seance-8.html"]')];
-  links.forEach(link => {
-    if (link.getAttribute("href") === "seance-7.html") link.setAttribute("href", "seance-7-v2.html");
-    if (link.getAttribute("href") === "seance-8.html") link.setAttribute("href", "seance-8-v2.html");
-  });
-})();
